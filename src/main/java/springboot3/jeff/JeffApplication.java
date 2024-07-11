acb327ccd5278b84468cc224ed0854a1f32a4092
GitHyeon <ryuoo0123@gmail.com>
2024-07-12 05:20:01 +0900

    first commit

---
+++ src/main/java/springboot3/jeff/JeffApplication.java
@@ -0,0 +1,13 @@
+package springboot3.jeff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeffApplication.class, args);
+	}
+
+}
