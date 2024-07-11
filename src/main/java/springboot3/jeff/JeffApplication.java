036a30f869f9e1ca5cea4d75d335bfce69e5308a
GitHyeon <ryuoo0123@gmail.com>
2024-07-12 05:35:12 +0900

    Setting : JPA 사용한 환경, application.yml 설정, db 설정

--- src/main/java/springboot3/jeff/JeffApplication.java
+++ src/main/java/springboot3/jeff/JeffApplication.java
@@ -2,8 +2,11 @@ package springboot3.jeff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JeffApplication {
 
 	public static void main(String[] args) {
